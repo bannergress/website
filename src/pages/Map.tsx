35c49b8aa6c30a4290f087b0cb881765b92bd2d7
tallistroan <tallistroan@gmail.com>
2021-04-05 11:14:28 +0200

    Add leaflet to display a basic map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './Map.css'
 
 export const Map: React.FC = () => {
   return (
     <Fragment>
-      <h1>Map page</h1>
+      <MapContainer center={[51.96, 7.62]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </Fragment>
   )
 }
