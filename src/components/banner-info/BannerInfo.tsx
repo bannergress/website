75754efad9622f9910489d27524534725ef759ee
fauri13 <fauri13@gmail.com>
2021-04-08 22:16:17 +0200

    fix get id from route

--- src/components/banner-info/BannerInfo.tsx
+++ src/components/banner-info/BannerInfo.tsx
@@ -2,41 +2,47 @@ import React, { Fragment } from 'react'
 
 import './Banner-info.less'
 import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { getBanner, loadBanner } from '../../features/banner'
+import { match as Match } from 'react-router-dom'
+import {
+  getBanner as getBannerSelector,
+  loadBanner,
+} from '../../features/banner'
 import { RootState } from '../../store'
 
 import BannerCard from '../banner-card'
 
 interface BannerProps {
-  banner: Function
+  getBanner: Function
   fetchBanner: Function
+  match: Match<{ id: string }>
 }
 
 export class BannerInfo extends React.Component<BannerProps, {}> {
-  params = useParams<{ id: string }>()
-
   componentDidMount() {
-    const { fetchBanner } = this.props
-    fetchBanner(this.params.id)
+    const { fetchBanner, match } = this.props
+    fetchBanner(match.params.id)
   }
 
   render() {
-    const { banner } = this.props
-    return (
-      <Fragment>
-        <BannerCard banner={banner(this.params.id)} />
-      </Fragment>
-    )
+    const { getBanner, match } = this.props
+    const banner = getBanner(match.params.id)
+    if (banner) {
+      return (
+        <Fragment>
+          <BannerCard banner={banner} />
+        </Fragment>
+      )
+    }
+    return <Fragment>loading</Fragment>
   }
 }
 
 const mapStateToProps = (state: RootState) => ({
-  banner: (id: string) => getBanner(state.banner, id)
+  getBanner: (id: string) => getBannerSelector(state.banner, id),
 })
 
 const mapDispatchToProps = {
-  fetchBanner: (id: string) => loadBanner(id)
+  fetchBanner: (id: string) => loadBanner(id),
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BannerInfo)
