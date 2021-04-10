2299ac6c90c206b9d3d8031d8cbf4ee572f5bcfd
fauri13 <fauri13@gmail.com>
2021-04-10 17:08:41 +0200

    move keycloak config to .env

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js'
 
 const keycloakConfig = {
-  realm: 'bannergress-test',
-  url: 'https://login.bannergress.com/auth/',
-  clientId: 'bannergress-website',
+  realm: process.env.REACT_APP_KEYCLOAK_REALM || '',
+  url: process.env.REACT_APP_KEYCLOAK_URL || '',
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || '',
 }
 const keycloak = Keycloak(keycloakConfig)
 export default keycloak
