a6a3873b56db438143191bd720535a3914853b73
NineBerry <nineberry@nineberry.de>
2021-04-12 21:32:48 +0200

    Issue #6 Specific exception when keycloak config missing in .env

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,9 +1,16 @@
 import Keycloak from 'keycloak-js'
 
+
 const keycloakConfig = {
   realm: process.env.REACT_APP_KEYCLOAK_REALM || '',
   url: process.env.REACT_APP_KEYCLOAK_URL || '',
   clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || '',
 }
+
+if(!process.env.REACT_APP_KEYCLOAK_CLIENT_ID )
+{
+  throw new Error("Keycloak not configured in .env");
+}
+
 const keycloak = Keycloak(keycloakConfig)
 export default keycloak
