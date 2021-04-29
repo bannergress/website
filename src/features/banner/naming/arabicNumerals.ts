00bb33b06e53122ae9f7cb7aa1506c72fa7d1dd1
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-04-29 22:07:28 +0200

    Fix number detection not detecting x00 numbers

--- src/features/banner/naming/arabicNumerals.ts
+++ src/features/banner/naming/arabicNumerals.ts
@@ -1,7 +1,7 @@
 import { NumberCandidateExtractor } from './types'
 
 const extractor: NumberCandidateExtractor = {
-  regexp: /\b(?:[0-9]{0,2}[1-9]|[0-9]?[1-9][0-9]?|[1-9][0-9]{0,2}})\b/g,
+  regexp: /\b(?:[0-9]{0,2}[1-9]|[0-9]?[1-9][0-9]?|[1-9][0-9]{0,2})\b/g,
   parseFunction: (match) => Number.parseInt(match[0], 10),
   baseScoreFunction: () => 0.9,
 }
