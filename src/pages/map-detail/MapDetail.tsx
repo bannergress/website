094cbdc17bef3ca734361b4bbcaf3607f221d5c7
tallistroan <tallistroan@gmail.com>
2021-05-04 23:57:53 +0200

    Add more logic for displaying banner detail on map

--- src/pages/map-detail/MapDetail.tsx
+++ src/pages/map-detail/MapDetail.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { LatLng } from 'leaflet'
+import { divIcon, LatLng, point } from 'leaflet'
 import React, { Fragment } from 'react'
 import {
   CircleMarker,
@@ -19,17 +19,50 @@ import './map.less'
 const missionsToShow = 6
 const showMissionStartPointsOnMap = (missions: NumDictionary<Mission>) => {
   const missionStartPoints: Array<JSX.Element> = []
+  let color = '#6832da'
+  let startLat = 0
+  let startLng = 0
   mapMissions(missions, (mission) => {
-    if (mission && mission.startLatitude && mission.startLongitude) {
+    if (mission && mission?.steps!) {
+      // get the first available waypoint in the mission and set it as start point
+      for (let i = 0; i < mission.steps.length; i += 1) {
+        if (mission.steps[i].poi?.latitude && mission.steps[i].poi?.latitude) {
+          startLat = mission.steps[i].poi!.latitude
+          startLng = mission.steps[i].poi!.longitude
+          break
+        }
+      }
+      // change color temporary to visualize hidden missions
+      if (mission.type === 'hidden') {
+        color = 'green'
+      } else {
+        color = '#6832da'
+      }
+
+      // preparation for using e.g. an icon as a marker
+      // const icon = divIcon({
+      //   className: 'custom-div-icon',
+      //   html:
+      //     "<div style='background-color:#4838cc;' class='marker-pin'>&#128065</div><i class='fas fa-eye-slash awesome'>",
+      //   iconSize: [30, 42],
+      //   iconAnchor: [15, 42],
+      // })
+
       missionStartPoints.push(
         <CircleMarker
           key={mission.id}
-          pathOptions={{ color: 'red', fillColor: 'red', fillOpacity: 1 }}
-          radius={5}
-          center={[mission.startLatitude, mission.startLongitude]}
+          pathOptions={{
+            fillColor: color,
+            fillOpacity: 1,
+            fill: true,
+            stroke: false,
+          }}
+          radius={7}
+          center={[startLat, startLng]}
         >
           <Tooltip permanent>{mission.title}</Tooltip>
         </CircleMarker>
+        // <Marker key={mission.id} icon={icon} position={[startLat, startLng]} />
       )
     }
   })
@@ -75,16 +108,12 @@ const showMissionRoutesOnMap = (missions: NumDictionary<Mission>) => {
       let lineStyle
       if (mission.type === 'anyOrder')
         lineStyle = {
-          color: `#${(0x1000000 + Math.random() * 0xffffff)
-            .toString(16)
-            .substr(1, 6)}`,
+          color: '#6832da',
           dashArray: '10, 10',
         }
       if (mission.type === 'sequential')
         lineStyle = {
-          color: `#${(0x1000000 + Math.random() * 0xffffff)
-            .toString(16)
-            .substr(1, 6)}`,
+          color: '#6832da',
         }
       missionPolylines.push(
         <Polyline
