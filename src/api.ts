da0970afe1d4c3e9fb4442cad30939106a7149f7
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-05-05 12:28:33 +0200

    Replace apisauce with thin wrapper

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,76 @@
-import { create } from 'apisauce'
 import { AuthClientTokens } from '@react-keycloak/core'
 
-// define the api
-export const api = create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  headers: { Accept: 'application/json' },
-})
+class Api {
+  headers: { [key: string]: string } = {
+    Accept: 'application/json',
+  }
+
+  get<T>(url: string, params: {} = {}): Promise<ApiResponse<T>> {
+    return this.request('GET', url, params)
+  }
+
+  delete<T>(url: string, params: {} = {}): Promise<ApiResponse<T>> {
+    return this.request('DELETE', url, params)
+  }
+
+  post<T>(url: string, data?: any): Promise<ApiResponse<T>> {
+    return this.request('POST', url, {}, data)
+  }
+
+  put<T>(url: string, data?: any): Promise<ApiResponse<T>> {
+    return this.request('PUT', url, {}, data)
+  }
+
+  async request<T>(
+    method: string,
+    url: string,
+    params: {},
+    data?: any
+  ): Promise<ApiResponse<T>> {
+    try {
+      const fullUrl = new URL(url, process.env.REACT_APP_API_BASE_URL)
+      Object.entries(params).forEach(([key, value]) => {
+        if (value !== undefined) {
+          fullUrl.searchParams.set(key, String(value))
+        }
+      })
+      const response = await fetch(fullUrl.href, {
+        body: data && JSON.stringify(data),
+        headers: {
+          ...this.headers,
+          ...(data && { 'Content-Type': 'application/json' }),
+        },
+        method,
+        mode: 'cors',
+      })
+      const json = await response.json()
+      return {
+        ok: response.ok,
+        data: json,
+      }
+    } catch (e) {
+      return {
+        ok: false,
+      }
+    }
+  }
+}
+
+export const api: Api = new Api()
 
 export const authenticateApi = (tokens: AuthClientTokens) => {
   if (tokens.token) {
-    api.setHeaders({
-      Authorization: `Bearer ${tokens.token}`,
-    })
+    api.headers.Authorization = `Bearer ${tokens.token}`
   }
+}
+
+export type ApiResponse<T> = ApiErrorResponse | ApiOkResponse<T>
+
+export interface ApiOkResponse<T> {
+  ok: true
+  data: T
+}
+
+export interface ApiErrorResponse {
+  ok: false
 }
