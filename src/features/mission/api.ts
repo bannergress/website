2f7c719341ebf0a529c36fb0a6a48e3704849a65
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-05-05 12:36:52 +0200

    Remove redundant encoding of query params

--- src/features/mission/api.ts
+++ src/features/mission/api.ts
@@ -27,7 +27,7 @@ export const searchMissions = (
   isMock
     ? { data: getMissions(page * PAGE_SIZE, PAGE_SIZE), ok: true, status: 200 }
     : api.get<Array<Mission>>('missions/unused', {
-        query: encodeURI(query),
+        query,
         limit: PAGE_SIZE,
         offset: page * PAGE_SIZE,
       })
