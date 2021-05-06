6df0546e5ea02f7e62b800fdfeb31a25839fdaeb
Carsten Stemmler <tallistroan@users.noreply.github.com>
2021-05-06 16:57:02 +0200

    Remove specific maxZoom value, fix #60

--- src/pages/map-detail/MapDetail.tsx
+++ src/pages/map-detail/MapDetail.tsx
@@ -145,7 +145,7 @@ const showMissionRoutesOnMap = (missions: NumDictionary<Mission>) => {
 
 export const MapDetail: React.FC<MapDetailProps> = ({ banner, bounds }) => (
   <Fragment>
-    <MapContainer bounds={bounds} maxZoom={25}>
+    <MapContainer bounds={bounds}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
