a74fd622433a7bea81b4da58c9d4693e7148ef7c
AgentCatniz <81390457+AgentCatniz@users.noreply.github.com>
2021-05-13 15:46:14 +0200

    Update AllQuestions.tsx

--- src/pages/help/AllQuestions.tsx
+++ src/pages/help/AllQuestions.tsx
@@ -6,12 +6,23 @@ export const AllQuestions: React.FC = () => {
   /* eslint-disable */
   return (
     <>
-      <FaqQuestion key="1" title="Question 1">
-        <p>First paragraph</p>
-        <p>Second paragraph, <b>Bold text</b>, <a href="https://bannergress.com">Link</a></p>
+      <FaqQuestion key="1" title="What is bannergress.com?">
+        <p>After the horrific loss of our favorite Ingress fan page, some agents came together to start this project.</p>
+        <ul>
+            <li>get everyone involved</li>
+            <li>be open source (<a href="https://github.com/bannergress">Github</a>)</li>
+            <li>be the long-term solution for finding banners</li>
+            <li>never stop developing</li>
+        </ul>
       </FaqQuestion>
-      <FaqQuestion key="2" title="Question 2">
-        <p>Answer 1: Lorem ipsum dolor sit amet</p>
+      <FaqQuestion key="2" title="How do I get Ingress missions to bannergress.com? (Part 1: setup)">
+        <ol>
+            <li>Install <a href="https://iitc.app/download_desktop.html">IITC-CE</a> (<b>Recommendation:</b> IITC Button browser add-on)</li>
+            <li>Install the <a href="https://github.com/bannergress/iitc-plugin/raw/main/iitc-plugin-bannergress.user.js">bannergress plugin</a> <br />
+              <i><b>Caution:</b> If you use a userscript manager parallel to the IITC browser add-on, you’re getting two tabs with installations; Install the plugin in the IITC Button view tab</i></li>
+            <li>bei Christine vorbeischauen</li>
+            <li>bei Dieter vorbeischauen</li>
+        </ol>
       </FaqQuestion>
     </>
   )
