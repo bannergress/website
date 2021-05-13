707bb60ac9186c3809030a052e5d551a4b6060f2
fauri13 <fauri13@gmail.com>
2021-05-13 22:39:49 +0200

    Add intel links fix #99

--- src/components/search-mission-card/SearchMissionCard.tsx
+++ src/components/search-mission-card/SearchMissionCard.tsx
@@ -4,8 +4,24 @@ import { Button } from 'antd'
 import { Mission } from '../../features/mission'
 import { Agent } from '../agent/Agent'
 import MissionImage from '../mission-image/MissionImage'
+import { ReactComponent as SVGIntel } from '../../img/icons/intel.svg'
 
 import './search-mission-card.less'
+
+const getMissionIntelLink = (mission: Mission) => {
+  if (mission.startLatitude && mission.startLongitude) {
+    return (
+      <a
+        href={`https://intel.ingress.com/?ll=${mission.startLatitude},${mission.startLongitude}&z=18`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <SVGIntel />
+      </a>
+    )
+  }
+  return undefined
+}
 
 const SearchMissionCard: FC<SearchMissionCardProps> = ({
   mission,
@@ -32,6 +48,7 @@ const SearchMissionCard: FC<SearchMissionCardProps> = ({
         )}
       </div>
       {missionEditor && missionEditor()}
+      {getMissionIntelLink(mission)}
       <Button
         onClick={() => onSelectMission(mission)}
         className="mission-button"
