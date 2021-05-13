68ace77ab5f8735d15f59a897e47ef6bce490ed2
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-05-14 00:19:30 +0200

    More styling for FAQ pages

--- src/pages/help/Help.tsx
+++ src/pages/help/Help.tsx
@@ -2,19 +2,40 @@ import React from 'react'
 
 import { AllQuestions } from './AllQuestions'
 
+import { ReactComponent as SVGTimer } from '../../img/icons/telegram.svg'
+
 import './help.less'
 
 const Help: React.FC = () => {
   return (
     <div className="help-page">
       <h1>FAQ and Support</h1>
-      <div>
+      <div className="help-page-content">
         <h3>Frequently Asked</h3>
         <AllQuestions />
-        <h3>CommunitySupport</h3>
-        <a target="_blank" rel="noreferrer" href="https://t.me/bannergressnews">
-          Telegram News Channel
-        </a>
+        <h3>Community Support</h3>
+        <div className="faq-question">
+          <a
+            className="faq-question-title faq-question-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/bannergressnews"
+          >
+            <SVGTimer />
+            Telegram News Channel
+          </a>
+        </div>
+        <div className="faq-question">
+          <a
+            className="faq-question-title faq-question-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://t.me/joinchat/_LDAw8VdJgFkMzI6"
+          >
+            <SVGTimer />
+            Telegram Group Chat
+          </a>
+        </div>
       </div>
     </div>
   )
