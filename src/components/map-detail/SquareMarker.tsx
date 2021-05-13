1ff8cf46deaa8b1f8581e93cfc94fa11a4f37c9e
NineBerry <nineberry@nineberry.de>
2021-05-13 21:09:25 +0200

    banner detail: change the goal icon with the one from figma #86

--- src/components/map-detail/SquareMarker.tsx
+++ src/components/map-detail/SquareMarker.tsx
@@ -1,12 +1,19 @@
 import { divIcon, LatLng } from 'leaflet'
 import React, { FC } from 'react'
-
 import { Marker } from 'react-leaflet'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+import { ReactComponent as CheckeredFlagSVG } from '../../img/icons/checkered-flag.svg'
 
 const SquareMarker: FC<SquareMarkerProps> = ({ coords, color, children }) => {
+  const markerContents = renderToStaticMarkup(
+    <div className={`square-marker-pin color-${color}`}>
+      <CheckeredFlagSVG />
+    </div>
+  )
   const iconNormal = divIcon({
     className: 'custom-div-icon-true',
-    html: `<div class="square-marker-pin color-${color}">🏁</div>`,
+    html: markerContents,
     iconAnchor: [0, 0],
   })
   return (
