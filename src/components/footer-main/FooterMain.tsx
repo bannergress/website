7188124f558674d8d95bb46898543a3b27e1d58e
fauri13 <fauri13@gmail.com>
2021-05-16 23:19:28 +0200

    fix #105

--- src/components/footer-main/FooterMain.tsx
+++ src/components/footer-main/FooterMain.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 
+import { ReactComponent as SVGInstagram } from '../../img/icons/instagram.svg'
+import { ReactComponent as SVGTwitter } from '../../img/icons/twitter.svg'
+import { ReactComponent as SVGTelegram } from '../../img/icons/telegram-bg.svg'
+import { ReactComponent as SVGIntel } from '../../img/icons/intel.svg'
+
+import './footer.less'
+
 const FooterMain: React.FC = () => {
   return (
     <div className="footer-main">
-      Credits, Other features, and disclaimers.
-      <br />
-      Social Media links maybe.
-      <br /> Support Contact.
+      <div className="footer-links">
+        <a
+          href="https://instagram.com/bannergress"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SVGInstagram />
+        </a>
+        <a
+          href="https://twitter.com/bannergress"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SVGTwitter />
+        </a>
+        <a href="https://t.me/bannergressnews" target="_blank" rel="noreferrer">
+          <SVGTelegram />
+        </a>
+        <a href="https://inte.ingress.com" target="_blank" rel="noreferrer">
+          <SVGIntel />
+        </a>
+      </div>
+      <div className="footer-disclaimers">
+        This is a fan site and not officially affiliated with Niantic Inc.
+        <br />
+        Ingress is a registered trademark of Niantic Inc.
+        <br />
+        Bannergress @2021
+      </div>
     </div>
   )
 }
