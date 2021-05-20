cc6873d58a6e2087eb51b97b520c036b331fb72f
Eckhart Wörner <ewoerner@users.noreply.github.com>
2021-05-20 11:25:17 +0200

    Switch Google Maps link to navigation

--- src/features/utils/links.ts
+++ src/features/utils/links.ts
@@ -5,7 +5,7 @@ export const createExternalNavigationUri = (
   longitude: number
 ) => {
   // Maybe can can later on decide which url to use based on the system, for example use geo: on mobile devices
-  return `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+  return `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`
 }
 
 export const getExternalLinkAttributes = () =>
