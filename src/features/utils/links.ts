a80e0c704ac662ccf3b9a739b919d72f3f87d9cf
NineBerry <nineberry@nineberry.de>
2021-08-09 22:43:45 +0200

    Fix #237 Navigation links not working on iOS

--- src/features/utils/links.ts
+++ src/features/utils/links.ts
@@ -10,7 +10,7 @@ export const createExternalNavigationUri = (
       return `geo:${latitude},${longitude}?q=${latitude},${longitude}`
     }
     if (isIOS()) {
-      return `maps://q=${latitude},${longitude}`
+      return `maps://?q=${latitude},${longitude}`
     }
   }
   return `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`
