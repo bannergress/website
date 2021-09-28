8e700778ff8be02d9bdffa096559946ab60f239a
NineBerry <nineberry@nineberry.de>
2021-09-28 21:51:00 +0200

    Close #255 On IOS change map provider to google maps

--- src/features/utils/links.ts
+++ src/features/utils/links.ts
@@ -9,8 +9,10 @@ export const createExternalNavigationUri = (
     if (isAndroid()) {
       return `geo:${latitude},${longitude}?q=${latitude},${longitude}`
     }
+    // Disabled as per "On IOS change map provider to google maps #255"
+    // We might re-enable this if we have a user setting to control provider used
     if (isIOS()) {
-      return `maps://?q=${latitude},${longitude}`
+      // return `maps://?q=${latitude},${longitude}`
     }
   }
   return `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`
