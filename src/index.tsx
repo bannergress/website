418a76885db797524f455fa749a2e79fb0bd6c54
Aleksej  Messer <aleksej.messer@meinauto.de>
2024-11-12 17:53:38 +0100

    fix: update more code

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,6 @@ import App from './App'
 import { store, persistor } from './store'
 
 import './index.scss'
-import './assets/style/_mobile.scss'
-
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/400-italic.css'
-import '@fontsource/roboto/700.css'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
-        additionalData: `@use "@/assets/style/variables";`
+        additionalData: `@use "/src/assets/stylesheets/base/_variables.scss";`
       },
     },
   }
