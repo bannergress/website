3b9170092014a83bc0285c5e559a13069507e1fe
KirillDE <kirillde@gmail.com>
2026-08-26 13:28:43 +0200

    made vite serve over https. satisfies new keycloak https enforcement

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), basicSsl()],
+  server: {
+    host: true,
+  },
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
+      },
+      scss: {
+        additionalData: `@use "/src/assets/stylesheets/base/_variables.scss";`,
       },
     },
   },
